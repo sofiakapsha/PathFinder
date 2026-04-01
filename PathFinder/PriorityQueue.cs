33b30f24f14b9c8d39a9461dc5f63d526c7b3404
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2026-04-01 18:40:51 +0300

    Added class PriorityQueue realized with min Heap

---
+++ PathFinder/PriorityQueue.cs
@@ -0,0 +1,81 @@
+﻿namespace PathFinder.MapGeneration;
+
+public class PriorityQueue
+{
+    private List<(Point, int)> _points = new List<(Point, int)>();
+
+    public int Count => _points.Count;
+
+    public bool TryDequeue(out Point point, out int priority)
+    {
+        point = default;
+        priority = default;
+        if (Count <= 0) return false;
+
+        var pair = Dequeue();
+        point = pair.Item1;
+        priority = pair.Item2;
+        return true;
+    }
+
+    public void Enqueue(Point point, int priority)
+    {
+        _points.Add((point, priority));
+
+        int index = Count - 1;
+
+        while (index > 0)
+        {
+            int parent = (index - 1) / 2;
+            if(_points[index].Item2 >= _points[parent].Item2) break;
+            
+            Swap(index, parent);
+            index = parent;
+        }
+       
+    }
+
+    public (Point, int) Dequeue()
+    {
+        var head = _points[0];
+
+        _points[0] = _points[Count - 1];
+        _points.RemoveAt(Count - 1);
+        
+        if(Count > 0) Heapify(0);
+
+        return head;
+    }
+
+    private void Heapify(int index)
+    {
+        while (true)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+
+            if (left < Count && _points[left].Item2 < _points[smallest].Item2)
+            {
+                smallest = left;
+            }
+            if (right < Count && _points[right].Item2 < _points[smallest].Item2)
+            {
+                smallest = right;
+            }
+            if (smallest == index) break;
+            
+            Swap(index, smallest);
+            index = smallest;
+        }
+    }
+
+    private void Swap(int i, int j)
+    {
+        var point = _points[i];
+        _points[i] = _points[j];
+        _points[j] = point;
+    }
+
+
+}
